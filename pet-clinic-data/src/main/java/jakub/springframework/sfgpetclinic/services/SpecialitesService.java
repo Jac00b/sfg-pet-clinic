40041fb5d23e65df8aa063081b6f3d3b51b3c31f
Jac00b <jakub.misiuk@gmail.com>
2020-11-15 00:33:13 +0100

    Added Specialites Map impl. Closes #31

---
+++ pet-clinic-data/src/main/java/jakub/springframework/sfgpetclinic/services/SpecialitesService.java
@@ -0,0 +1,7 @@
+package jakub.springframework.sfgpetclinic.services;
+
+import jakub.springframework.sfgpetclinic.model.Speciality;
+
+public interface SpecialitesService extends CrudService<Speciality, Long> {
+
+}
