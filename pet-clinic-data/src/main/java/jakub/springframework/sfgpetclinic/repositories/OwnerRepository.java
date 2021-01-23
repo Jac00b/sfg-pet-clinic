acfaed7c8f7b5ba6a0e3b1877b1a9c12b20fc295
Jac00b <jakub.misiuk@gmail.com>
2021-01-23 21:35:48 +0100

    Implement Find Owner . Closes #54

--- pet-clinic-data/src/main/java/jakub/springframework/sfgpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/jakub/springframework/sfgpetclinic/repositories/OwnerRepository.java
@@ -3,8 +3,12 @@ package jakub.springframework.sfgpetclinic.repositories;
 import jakub.springframework.sfgpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
 
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
 
 }
--- pet-clinic-data/src/main/java/jakub/springframework/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/jakub/springframework/sfgpetclinic/services/OwnerService.java
@@ -2,8 +2,12 @@ package jakub.springframework.sfgpetclinic.services;
 
 import jakub.springframework.sfgpetclinic.model.Owner;
 
+import java.util.List;
+
 public interface OwnerService extends CrudService<Owner, Long>{
 
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
 
 }
